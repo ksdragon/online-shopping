ad151794fbabe14ca72532418658fe3d5f197d85
Krzysztof Skoneczny <ksdragon@poczta.pl>
2018-10-25 11:06:44 +0200

    My Project Setup Complete

---
+++ online_shoppingBackEnd_sBs/src/main/java/pl/skoneczny/stepByStepApllication/online_shoppingBackEnd_sBs/App.java
@@ -0,0 +1,13 @@
+package pl.skoneczny.stepByStepApllication.online_shoppingBackEnd_sBs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
